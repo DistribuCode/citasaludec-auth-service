58f924c0fb3c14b057067faad2dfe967c192d955
Jeff97ares <92552303+Jeff97ares@users.noreply.github.com>
2025-07-12 23:58:27 -0500

    chore: remove unused backend folder

--- backend/src/routes/medicalHistory.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/medicalHistory.controller');
-
-router.post('/', controller.crearHistorial);
-router.get('/', controller.obtenerHistoriales);
-
-module.exports = router;
